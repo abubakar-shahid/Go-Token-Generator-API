1d6964e8a2671fdb1f0f81e231a0e2a579b88dba
Abubakar Shahid <abubakarshahid1309@gmail.com>
2024-07-04 19:59:59 +0500

    Project Removed from Vercel

--- go.mod
+++ go.mod
@@ -2,4 +2,4 @@ module generate_token
 
 go 1.22.4
 
-require github.com/dgrijalva/jwt-go v3.2.0+incompatible // indirect
+require github.com/dgrijalva/jwt-go v3.2.0+incompatible
--- main.go
+++ main.go
@@ -1,13 +1,65 @@
 package main
 
 import (
-	"Go-Token-Generator-API/api/handler" // Ensure the correct import path
+	"encoding/json"
 	"fmt"
+	"log"
 	"net/http"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
 )
 
+type Response struct {
+	Token string `json:"token"`
+}
+
+type RequestBody struct {
+	RoomName string `json:"roomName"`
+	UserInfo struct {
+		APIKey    string `json:"apiKey"`
+		APISecret string `json:"apiSecret"`
+	} `json:"userInfo"`
+}
+
+func GenerateToken(apiKey, apiSecret string) (string, error) {
+	token := jwt.New(jwt.SigningMethodHS256)
+
+	claims := token.Claims.(jwt.MapClaims)
+
+	claims["apiKey"] = apiKey
+	claims["exp"] = time.Now().Add(time.Minute * 30).Unix()
+
+	tokenString, err := token.SignedString([]byte(apiSecret))
+
+	if err != nil {
+		fmt.Errorf("Something Went Wrong: %s", err.Error())
+		return "", err
+	}
+
+	return tokenString, nil
+}
+
+func GetTokenHandler(w http.ResponseWriter, r *http.Request) {
+	var requestBody RequestBody
+	err := json.NewDecoder(r.Body).Decode(&requestBody)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusBadRequest)
+		return
+	}
+
+	tokenString, err := GenerateToken(requestBody.UserInfo.APIKey, requestBody.UserInfo.APISecret)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	response := Response{Token: tokenString}
+	json.NewEncoder(w).Encode(response)
+}
+
 func main() {
-	http.HandleFunc("/api/get-token", handler.GetTokenHandler)
-	fmt.Println("Server running at port 8080...")
+	http.HandleFunc("/get-token", GetTokenHandler)
+	log.Println("Server running on port 8080...")
 	http.ListenAndServe(":8080", nil)
 }
