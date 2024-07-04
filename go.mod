2a4989bef76f6f643491ab13a08edb5e7b5f6c76
Abubakar Shahid <abubakarshahid1309@gmail.com>
2024-07-05 03:04:41 +0500

    Updated to https

--- go.mod
+++ go.mod
@@ -2,6 +2,7 @@ module generate_token
 
 go 1.22.4
 
-require github.com/dgrijalva/jwt-go v3.2.0+incompatible
-
-require github.com/rs/cors v1.11.0 // indirect
+require (
+	github.com/golang-jwt/jwt/v5 v5.2.1 // indirect
+	github.com/rs/cors v1.11.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,3 +1,84 @@
+// package main
+
+// import (
+// 	"encoding/json"
+// 	"fmt"
+// 	"log"
+// 	"net/http"
+// 	"time"
+
+// 	"github.com/dgrijalva/jwt-go"
+// 	"github.com/rs/cors"
+// )
+
+// type Response struct {
+// 	Token string `json:"token"`
+// }
+
+// type UserInfo struct {
+// 	Identity string `json:"identity"`
+// 	Name     string `json:"name"`
+// 	Metadata string `json:"metadata"`
+// }
+
+// type RequestBody struct {
+// 	RoomName string   `json:"roomName"`
+// 	UserInfo UserInfo `json:"userInfo"`
+// }
+
+// func GenerateToken(userInfo UserInfo) (string, error) {
+// 	token := jwt.New(jwt.SigningMethodHS256)
+
+// 	claims := token.Claims.(jwt.MapClaims)
+
+// 	claims["identity"] = userInfo.Identity
+// 	claims["name"] = userInfo.Name
+// 	claims["metadata"] = userInfo.Metadata
+// 	claims["exp"] = time.Now().Add(time.Minute * 30).Unix()
+
+// 	tokenString, err := token.SignedString([]byte("secret"))
+
+// 	if err != nil {
+// 		fmt.Errorf("Something Went Wrong: %s", err.Error())
+// 		return "", err
+// 	}
+
+// 	return tokenString, nil
+// }
+
+// func GetTokenHandler(w http.ResponseWriter, r *http.Request) {
+// 	log.Println("Client Requested for Token:")
+// 	var requestBody RequestBody
+// 	err := json.NewDecoder(r.Body).Decode(&requestBody)
+// 	log.Println("data received: ", requestBody)
+// 	if err != nil {
+// 		http.Error(w, err.Error(), http.StatusBadRequest)
+// 		return
+// 	}
+
+// 	tokenString, err := GenerateToken(requestBody.UserInfo)
+// 	if err != nil {
+// 		http.Error(w, err.Error(), http.StatusInternalServerError)
+// 		return
+// 	}
+
+// 	response := Response{Token: tokenString}
+// 	json.NewEncoder(w).Encode(response)
+// 	log.Println("Token Sent!")
+// }
+
+// func main() {
+// 	c := cors.New(cors.Options{
+// 		AllowedOrigins: []string{"http://localhost:3000"},
+// 		AllowedMethods: []string{http.MethodPost},
+// 		AllowedHeaders: []string{"Content-Type"},
+// 	})
+
+// 	http.Handle("/get-token", c.Handler(http.HandlerFunc(GetTokenHandler)))
+// 	log.Println("Server running on port 8080...")
+// 	http.ListenAndServe(":8080", nil)
+// }
+
 package main
 
 import (
@@ -5,9 +86,10 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"os"
 	"time"
 
-	"github.com/dgrijalva/jwt-go"
+	"github.com/golang-jwt/jwt/v5"
 	"github.com/rs/cors"
 )
 
@@ -15,49 +97,59 @@ type Response struct {
 	Token string `json:"token"`
 }
 
-type RequestBody struct {
-	RoomName string `json:"roomName"`
-	UserInfo struct {
-		APIKey    string `json:"apiKey"`
-		APISecret string `json:"apiSecret"`
-	} `json:"userInfo"`
+type UserInfo struct {
+	Identity string `json:"identity"`
+	Name     string `json:"name"`
+	Metadata string `json:"metadata"`
 }
 
-func GenerateToken(apiKey, apiSecret string) (string, error) {
-	token := jwt.New(jwt.SigningMethodHS256)
+type RequestBody struct {
+	RoomName string   `json:"roomName"`
+	UserInfo UserInfo `json:"userInfo"`
+}
 
-	claims := token.Claims.(jwt.MapClaims)
+var (
+	apiKey    = os.Getenv("devkey")
+	apiSecret = os.Getenv("secret")
+)
 
-	claims["apiKey"] = apiKey
-	claims["exp"] = time.Now().Add(time.Minute * 30).Unix()
+func GenerateToken(userInfo UserInfo, apiKey, apiSecret string) (string, error) {
+	token := jwt.NewWithClaims(jwt.SigningMethodHS256, jwt.MapClaims{
+		"identity": userInfo.Identity,
+		"name":     userInfo.Name,
+		"metadata": userInfo.Metadata,
+		"exp":      time.Now().Add(time.Minute * 30).Unix(),
+		"apiKey":   apiKey,
+	})
 
 	tokenString, err := token.SignedString([]byte(apiSecret))
-
 	if err != nil {
-		fmt.Errorf("Something Went Wrong: %s", err.Error())
-		return "", err
+		return "", fmt.Errorf("Failed to generate token: %v", err)
 	}
 
-	log.Println("Token Sent!")
 	return tokenString, nil
 }
 
 func GetTokenHandler(w http.ResponseWriter, r *http.Request) {
+	log.Println("Client requested for token:")
 	var requestBody RequestBody
 	err := json.NewDecoder(r.Body).Decode(&requestBody)
+	log.Println("data received: ", requestBody)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusBadRequest)
 		return
 	}
 
-	tokenString, err := GenerateToken(requestBody.UserInfo.APIKey, requestBody.UserInfo.APISecret)
+	tokenString, err := GenerateToken(requestBody.UserInfo, apiKey, apiSecret)
 	if err != nil {
 		http.Error(w, err.Error(), http.StatusInternalServerError)
 		return
 	}
 
 	response := Response{Token: tokenString}
+	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(response)
+	log.Println("Token sent!")
 }
 
 func main() {
@@ -67,7 +159,13 @@ func main() {
 		AllowedHeaders: []string{"Content-Type"},
 	})
 
-	http.Handle("/get-token", c.Handler(http.HandlerFunc(GetTokenHandler)))
-	log.Println("Server running on port 8080...")
-	http.ListenAndServe(":8080", nil)
+	certFile := "ssl/cert.pem"
+	keyFile := "ssl/key.pem"
+
+	http.Handle("/generate-token", c.Handler(http.HandlerFunc(GetTokenHandler)))
+
+	log.Println("Server running on https://localhost:8443...")
+	if err := http.ListenAndServeTLS(":8443", certFile, keyFile, nil); err != nil {
+		log.Fatalf("Failed to start server: %v", err)
+	}
 }
